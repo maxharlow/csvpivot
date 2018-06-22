02ede094f588e0e670fd256c252b99a369a97791
Max Harlow <maxharlow@gmail.com>
2018-06-22 21:57:14 +0100

    Bump version

--- tests.py
+++ tests.py
@@ -2,19 +2,36 @@
 
 import csvpivot
 
-def test_simple():
-    headers = ['name', 'location', 'population']
+def test_mean():
+    headers = ['location', 'name', 'population']
     data = [
-        ['Bermuda', 'North Atlantic', 64000],
-        ['Cayman Islands', 'Caribbean', 56000],
-        ['Turks and Caicos Islands', 'North Atlantic', 32000],
-        ['Gibraltar', 'Europe', 28800],
-        ['British Virgin Islands', 'Caribbean', 27000]
+        ['Caribbean', 'Cayman Islands', 56000],
+        ['Caribbean', 'British Virgin Islands', 27000],
+        ['North Atlantic', 'Bermuda', 64000],
+        ['North Atlantic', 'Turks and Caicos Islands', 32000],
+        ['Europe', 'Gibraltar', 28800],
     ]
     results, keys = csvpivot.run(data, headers, rows=['location'], values=['mean(population)'])
     assert keys == ['location', 'mean(population)']
     assert list(results) == [
         ['Caribbean', 41500],
-        ['Europe', 28800],
-        ['North Atlantic', 48000]
+        ['North Atlantic', 48000],
+        ['Europe', 28800]
+    ]
+
+def test_median():
+    headers = ['location', 'name', 'population']
+    data = [
+        ['Caribbean', 'Cayman Islands', 56000],
+        ['Caribbean', 'British Virgin Islands', 27000],
+        ['North Atlantic', 'Bermuda', 64000],
+        ['North Atlantic', 'Turks and Caicos Islands', 32000],
+        ['Europe', 'Gibraltar', 28800],
+    ]
+    results, keys = csvpivot.run(data, headers, rows=['location'], values=['median(population)'])
+    assert keys == ['location', 'median(population)']
+    assert list(results) == [
+        ['Caribbean', 41500],
+        ['North Atlantic', 48000],
+        ['Europe', 28800]
     ]
